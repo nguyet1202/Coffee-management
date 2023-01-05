01678067665328ff8b4bd080713c0c381a0da861
Nguyet <nguyet.ho23@student.passerellesnumeriques.org>
2023-01-05 22:54:40 +0700

    show data

--- App.js
+++ App.js
@@ -1,119 +1,156 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import type {Node} from 'react';
-import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  FlatList,
   View,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
 } from 'react-native';
+import React, {useEffect} from 'react';
+import useFirestoreCollection from './src/hooks/useFirestoreCollection';
+// import firestore from '@react-native-firebase/firestore';
+import firestore from '@react-native-firebase/firestore';
+// import {firebase} from '@react-native-firebase/firestore';
 
-// import {Icon} from 'react-native-vector-icons/Entypo';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      {/* <Icon name="aircraft" size={30} color="black" /> */}
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+const App = () => {
+  // const collection = firestore().collection('products');
+  const collection = firestore().collection('products');
+  const pageSize = 5;
+  const page = 2;
+  const {data, loading, error, refresh} = useFirestoreCollection(
+    collection,
+    pageSize,
+    page,
   );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    refresh();
+  }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  }
+  const renderItem = ({item}) => {
+    return (
+      <TouchableOpacity style={styles.item}>
+        <Image style={styles.img} source={{uri: item.img}} />
+        <TouchableOpacity style={styles.tilte}>
+          <Text style={styles.tilteText}>{item?.name}</Text>
+          <View style={styles.buttoninfor}>
+            <TouchableOpacity style={styles.smallinfor}>
+              <Image
+                source={{
+                  uri: 'https://xuonginthanhpho.com/wp-content/uploads/2020/03/map-marker-icon.png',
+                }}
+                style={{width: 12, height: 12, marginTop: 1}}
+              />
+              <Text
+                style={{color: '#000000', fontSize: 10, fontWeight: 'bold'}}>
+                {item?.price}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.smallinfor}>
+              <Image
+                source={{
+                  uri: 'https://newtechco.com.vn/wp-content/uploads/2019/06/icon-dung-thoigian.png',
+                }}
+                style={{width: 12, height: 12, marginRight: 3}}
+              />
+              <Text
+                style={{color: '#000000', fontSize: 10, fontWeight: 'bold'}}>
+                {item?.ingredients}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      {loading ? (
+        <ActivityIndicator color="#00ff00" size="large" />
+      ) : (
+        <FlatList
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          onRefresh={refresh}
+          refreshing={loading}
+        />
+      )}
+    </>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    width: '92%',
+    marginLeft: 15,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  contentContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  img: {
+    width: 170,
+    height: 220,
+    borderRadius: 15,
+    opacity: 0.9,
   },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    position: 'relative',
+    width: 180,
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  tilte: {
+    position: 'absolute',
+    top: 150,
+    left: 10,
+    textAlign: 'center',
+    width: 150,
+    height: 60,
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    opacity: 2,
+    borderRadius: 7,
+  },
+  tilteText: {
+    textAlign: 'center',
+    color: '#044040',
+    fontSize: 12,
+    fontWeight: 'bold',
+    paddingTop: 6,
+  },
+  emptyMessageStyle: {
+    textAlign: 'center',
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+  },
+  buttoninfor: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  smallinfor: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    paddingLeft: 3,
+    paddingRight: 3,
   },
 });
-
-export default App;
