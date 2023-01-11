4f451e9af665c30d8d54938bc4b7f8d4c13b428c
Nguyet <nguyet.ho23@student.passerellesnumeriques.org>
2023-01-12 01:26:22 +0700

    update package.json

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import LoginScreen from './src/screens/LoginScreen';
-import Homepage from './src/screens/Homepage';
 
-const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Login"
-          component={LoginScreen}
-        />
-        <Stack.Screen name="Home" component={Homepage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
